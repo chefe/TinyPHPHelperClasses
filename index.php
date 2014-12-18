70b870bd149dd2e40df7d19c8e651ae1ec77f069
chefe <chefe@users.noreply.github.com>
2014-12-18 12:10:02 +0100

    Update index.php
    Format code and improve comments

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
 
-// Include File
-require_once('template.class.php');
-
-// Create a new Template
-$tpl = new Template('page.tpl');
-
-// Fill it up with values
-$tpl->assign('title', 'TinyTemplateEngine');
-$tpl->assign('subtitle', 'A small one file php template engine');
-$tpl->assign('looptitle', 'Now comes a loop:');
-$peoples = array();
-$peoples[0] = array('name' => 'miller', 'surname' => 'peter');
-$peoples[1] = array('name' => 'stone', 'surname' => 'frank');
-$tpl->assign('people', $peoples);
-$tpl->assign('footer', 'This is the end of the template!');
-
-// Display the template
-$tpl->display();
+  // Include File
+  require_once('template.class.php');
+  
+  // Create a new Template
+  $tpl = new Template('page.tpl');
+  
+  // Build up data structure for template
+  $peoples = array();
+  $peoples[0] = array('name' => 'miller', 'surname' => 'peter');
+  $peoples[1] = array('name' => 'stone', 'surname' => 'frank');
+  
+  // Fill up template with values
+  $tpl->assign('title', 'TinyTemplateEngine');
+  $tpl->assign('subtitle', 'A small one file php template engine');
+  $tpl->assign('looptitle', 'Now comes a loop:');
+  $tpl->assign('people', $peoples);
+  $tpl->assign('footer', 'This is the end of the template!');
+  
+  // Display the template
+  $tpl->display();
 
 ?>
